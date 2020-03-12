a38abf0d8660480ece070902420f56300d6fd226
inferman <infernal.manowar@gmail.com>
2020-03-12 16:58:54 +0200

    refreshing data with subject

--- src/app/band-list/band-list.component.ts
+++ src/app/band-list/band-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable, Subject } from "rxjs";
+import {map, mergeMap, startWith} from 'rxjs/operators';
 
 // import { AutoUnsubscribe } from "../../decorators/auto-unsubscribe";
 
 import { BandDataService } from "../band-data.service";
 import { Band } from "../model";
-import { shareReplay } from 'rxjs/operators';
+import { shareReplay } from "rxjs/operators";
 
 @Component({
   selector: "app-band-list",
@@ -13,11 +14,20 @@ import { shareReplay } from 'rxjs/operators';
 })
 export class BandListComponent implements OnInit, OnDestroy {
   bandList$: Observable<Band[]>;
+  refreshClickSubject$: Subject<any> = new Subject();
 
   constructor(private bandDataService: BandDataService) {}
 
   ngOnInit() {
-    this.bandList$ = this.bandDataService.getBands().pipe(shareReplay());
+    const refreshDataClick$ = this.refreshClickSubject$.asObservable();
+    this.bandList$ = refreshDataClick$.pipe(
+      startWith({}),
+      mergeMap(_ => this.getData())
+    )
+  }
+
+  private getData(): Observable<Band[]> {
+    return this.bandDataService.getBands().pipe(shareReplay());
   }
 
   ngOnDestroy() {}
